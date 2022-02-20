1803d1027def570cb2b4ec310a11b8aa6b69d71d
HEIXAIYUE <q1506589520q@gmail.com>
2022-02-21 01:28:55 +0800

    02-原型链继承

--- 02-原型链继承.js
+++ 02-原型链继承.js
@@ -16,3 +16,7 @@ console.log(color1.getColor()); // [ 'red', 'block' ]
 color1.pushColor("yellow");
 let color2 = new color();
 console.log(color2.getColor()); // [ 'red', 'block', 'yellow' ]
+
+// 原型链继承存在的问题：
+// 问题1：原型中包含的引用类型属性将被所有实例共享；
+// 问题2：子类在实例化的时候不能给父类构造函数传参；
